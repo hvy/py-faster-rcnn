2d4c491bed78d96b919bf21a6359b82e6993c1c0
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2016-05-24 15:36:44 +0000

    Add dataset/file utils

---
+++ yamlab/utils/datasetutil.py
@@ -0,0 +1,76 @@
+from utils import fileutil
+
+
+def filename_to_id(filename):
+    # TODO: Use regexp (re module) instead of splits
+    id_str = filename.split('.')[0].split('_')[2]
+    id_int = int(id_str)
+    return id_int
+
+
+def row_to_annotations(row):
+    """Take a list of values in a csv file row and return a list of annotations.
+    """
+    image_filename = row.pop(0)
+    image_id = filename_to_id(image_filename)
+
+    n_props = 6  # annotation id, category id, upper left x, upper left y, bottom right x, bottom right y
+    assert len(row) % n_props == 0
+    n_anns = len(row) / n_props
+
+    annotations = []
+
+    for ann_i in range(n_anns):
+        offset = ann_i * n_props
+        subrow = row[offset:offset + n_props]
+        subrow = map(int, subrow)
+
+        ann_id = subrow[0]
+        category_id = subrow[1]
+        upper_left_x = subrow[2]
+        upper_left_y = subrow[3]
+        bottom_right_x = subrow[4]
+        bottom_right_y = subrow[5]
+
+        assert (isinstance(upper_left_x, int) and upper_left_x >= 0)
+        assert (isinstance(upper_left_y, int) and upper_left_y >= 0)
+        assert (isinstance(bottom_right_x, int) and bottom_right_x > 0)
+        assert (isinstance(bottom_right_y, int) and bottom_right_y > 0)
+
+        assert bottom_right_x > upper_left_x
+        assert bottom_right_y > upper_left_y
+
+        width = bottom_right_x - upper_left_x
+        height = bottom_right_y - upper_left_y
+        area = width * height
+
+        assert area > 0
+
+        bbox = [upper_left_x, upper_left_y, width, height]
+
+        # TODO: Add 'segmentation' to each annotation, if necessary
+        # See format here http://mscoco.org/dataset/#download
+        annotation = {'id': ann_id, 'image_id': image_id, 'category_id': category_id, 'area': area, 'bbox': bbox, 'iscrowd': 0}
+
+        annotations.append(annotation)
+
+    return annotations
+
+
+def annotations_from_csv(filename):
+    # Read the csv file
+    csvfile = fileutil.read_csv(filename)
+
+    # Process the csv file
+    annotations = []
+    for row in csvfile:
+        trailing = row.pop()  # Remove last value from each row caused by trailing comma
+        assert trailing == ''
+
+        # Parse the row to an annotation
+        annotations_in_row = row_to_annotations(row)
+        annotations += annotations_in_row
+
+    print 'Total annotations: {}'.format(len(annotations))
+
+    return annotations
---
+++ yamlab/utils/fileutil.py
@@ -0,0 +1,50 @@
+import csv
+import json
+
+
+def read_csv(filename, delimiter=','):
+    """Return a 2-dimensional list where each row represents a row in the
+    csv file and each column represents each value in the row.
+    """
+    rows = []
+    with open(filename, 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter=delimiter)
+        for row in reader:
+            rows.append(row)
+    return rows
+
+
+def read_json(filename):
+    """Read JSON file as a dict.
+    """
+    json_as_dict = None
+    with open(filename, 'rb') as jsonfile:
+        json_as_dict = json.load(jsonfile)
+    return json_as_dict
+
+
+def write_json(filename, data, indent=None):
+    """Write dict as JSON.
+    Args:
+        indent (int): Indentation used for pretty printing.
+    """
+    with open(filename, 'w') as jsonfile:
+        if indent is None:
+            json.dump(data, jsonfile, indent=indent)
+        else:
+            json.dump(data, jsonfile)
+
+
+if __name__ == '__main__':
+    print('Loading data...')
+    # val_data_dict = read_json('/home/ubuntu/data/MSCOCO/annotations/instances_val2014.json')
+    val_data_dict = read_json('./new_val_data.json')
+    print('Done loading data')
+
+    annotations = val_data_dict['annotations']
+    # val_data_dict['annotations'] = ['hey']
+
+    write_json('new_val_data.json', val_data_dict)
+
+    print('Number of annotations: {}'.format(len(annotations)))
+
